cb18f6152cf8debc6580e0fdb2a43fcb08d0b886
AndreFeMarques <1200605@isep.ipp.pt>
2024-12-08 00:40:47 +0000

    env variables

--- src/main/java/tattoowebsite/tattobackend/config/GoogleSheetsConfig.java
+++ src/main/java/tattoowebsite/tattobackend/config/GoogleSheetsConfig.java
@@ -6,11 +6,10 @@ import com.google.api.client.json.JsonFactory;
 import com.google.api.client.json.jackson2.JacksonFactory;
 import com.google.api.services.sheets.v4.Sheets;
 import com.google.api.services.sheets.v4.SheetsScopes;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-import java.io.FileInputStream;
+import java.io.ByteArrayInputStream;
 import java.io.IOException;
 import java.security.GeneralSecurityException;
 import java.util.Collections;
@@ -18,15 +17,21 @@ import java.util.Collections;
 @Configuration
 public class GoogleSheetsConfig {
 
-    @Value("${google.sheets.credentials.path}")
-    private String credentialsPath;
-
     @Bean
     public Sheets sheetsService() throws IOException, GeneralSecurityException {
-        FileInputStream serviceAccountStream = new FileInputStream(credentialsPath);
-        GoogleCredential credential = GoogleCredential.fromStream(serviceAccountStream)
+        // Lê o JSON de credenciais da variável de ambiente
+        String credentialsJson = System.getenv("GOOGLE_CREDENTIALS");
+        if (credentialsJson == null || credentialsJson.isEmpty()) {
+            throw new IllegalStateException("Variável de ambiente GOOGLE_CREDENTIALS não configurada ou vazia.");
+        }
+
+        // Converte o JSON de credenciais em um stream
+        GoogleCredential credential = GoogleCredential.fromStream(
+                new ByteArrayInputStream(credentialsJson.getBytes()))
                 .createScoped(Collections.singleton(SheetsScopes.SPREADSHEETS));
+        
         JsonFactory jsonFactory = JacksonFactory.getDefaultInstance();
+
         return new Sheets.Builder(GoogleNetHttpTransport.newTrustedTransport(), jsonFactory, credential)
                 .setApplicationName("TattooBackend")
                 .build();
