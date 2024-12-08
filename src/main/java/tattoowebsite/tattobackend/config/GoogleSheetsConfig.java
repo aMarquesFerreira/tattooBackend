7c102eb4cbf1aeae4dd3144a75dc35847af8d39a
AndreFeMarques <1200605@isep.ipp.pt>
2024-12-08 01:05:46 +0000

    outra tentativa

--- src/main/java/tattoowebsite/tattobackend/config/GoogleSheetsConfig.java
+++ src/main/java/tattoowebsite/tattobackend/config/GoogleSheetsConfig.java
@@ -19,27 +19,29 @@ public class GoogleSheetsConfig {
 
     @Bean
     public Sheets sheetsService() throws IOException, GeneralSecurityException {
-        // Lê o JSON de credenciais da variável de ambiente
-        String credentialsJson = System.getenv("GOOGLE_CREDENTIALS");
         System.out.println("Iniciando configuração do Google Sheets.");
+        
+        String credentialsJson = System.getenv("GOOGLE_CREDENTIALS");
         System.out.println("Verificando variável GOOGLE_CREDENTIALS...");
+
         if (credentialsJson == null || credentialsJson.isEmpty()) {
             System.out.println("Erro: Variável GOOGLE_CREDENTIALS não está configurada ou está vazia.");
             throw new IllegalStateException("Variável de ambiente GOOGLE_CREDENTIALS não configurada ou vazia.");
         }
+        
         System.out.println("Credenciais carregadas com sucesso.");
-
-        // Adicione este log para verificar se o JSON está sendo carregado corretamente
-        System.out.println("Credenciais lidas: " + credentialsJson);
 
         // Converte o JSON de credenciais em um stream
         GoogleCredential credential = GoogleCredential.fromStream(
                 new ByteArrayInputStream(credentialsJson.getBytes()))
                 .createScoped(Collections.singleton(SheetsScopes.SPREADSHEETS));
-        
+
         JsonFactory jsonFactory = JacksonFactory.getDefaultInstance();
 
-        return new Sheets.Builder(GoogleNetHttpTransport.newTrustedTransport(), jsonFactory, credential)
+        return new Sheets.Builder(
+                GoogleNetHttpTransport.newTrustedTransport(), 
+                jsonFactory, 
+                credential)
                 .setApplicationName("TattooBackend")
                 .build();
     }
